8f63b8f8d40e5923fab59500d8417b05f35494a1
Munder2 <munderlifew11@gmail.com>
2026-04-29 13:47:06 +0300

    Implement PyLib installer script

---
+++ pl_installer.py
@@ -0,0 +1,30 @@
+import pathlib
+print('Welcome to the PyLib Installer!')
+print('First, choose the folder to install PyLib.')
+folder = input('folder>> ')
+pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+with open(folder+'/pl.py', 'w') as f:
+    f.write(f'''import requests as r
+import sys
+
+def __install(package):
+    fd = '{folder}'
+    url = 'https://github.com/Munder2/PyLib/raw/refs/heads/main/libs/'+package
+    resp = r.get(url)
+    print('Getting the package . . .')
+    if resp.status_code == 200:
+        print('Success!\nDownloading the package . . .')
+        with open(fd+package, 'wb') as f:
+            f.write(resp.content)
+        print('Success!')
+    else:
+        print('Failed to get the package.\nTry looking for errors in the enterred package name.'); sys.exit(1)
+
+if __name__ == '__main__':
+    try:
+        if sys.argv[1] == '-d':
+            __install()
+        else:
+            print('Usage: pl -d <package>'); sys.exit(1)
+    except:
+        print('Usage: pl -d <package>'); sys.exit(1)''')
